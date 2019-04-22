b2b8473606caed2598370f13973e3a910a38b8cd
undert0w <608012+undert0w@users.noreply.github.com>
2019-04-21 19:56:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blockchain Example
+
+Just a simple example of creating a blockchain and mining new blocks.
---
+++ blockchain.py
@@ -0,0 +1,66 @@
+import datetime
+import hashlib
+
+
+class Block:
+    block_number = 0
+    data = None
+    next_block = None
+    current_hash = None
+    nonce = 0
+    previous_hash = 0x0
+    timestamp = datetime.datetime.now()
+
+    def __init__(self, data):
+        self.data = data
+
+    def hash(self):
+        h = hashlib.sha256()
+        h.update(
+            str(self.nonce).encode('utf-8') + 
+            str(self.data).encode('utf-8') + 
+            str(self.previous_hash).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') + 
+            str(self.block_number).encode('utf-8')
+        ) 
+        return h.hexdigest()
+
+    def __str__(self):
+        return "Block Hash: " + str(self.hash()) + \
+            "\nBlock #: " + str(self.block_number) + \
+            "\nBlock Data: " + str(self.data) + \
+            "\nHashes: " + str(self.nonce) + \
+            "\n--------------------------------------------"
+
+class Blockchain:
+    max_nonce = 2**32
+    diff = 10
+    target = 2**(256-diff)
+
+    block = Block("Genesis")
+    head = block
+    
+    def add(self, block):
+        block.previous_hash = self.block.hash()
+        block.block_number = self.block.block_number + 1
+
+        self.block.next_block = block
+        self.block = self.block.next_block
+
+    def mine(self, block):
+        for n in range(self.max_nonce):
+            if int(block.hash(), 16) <= self.target:
+                self.add(block)
+                print(block)
+                break
+            else:
+                block.nonce += 1
+
+blockchain = Blockchain()
+for n in range(10):
+    blockchain.mine(Block("Block " + str(n + 1)))
+
+while blockchain.head != None:
+    print(blockchain.head)
+    blockchain.head = blockchain.head.next_block
+
